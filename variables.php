0ae058c8d81d4c757c655a759fc4c2080b7e9dd2
Andres Villacres <andreslkf14@gmail.com>
2023-01-25 01:31:47 -0500

    primitivas

---
+++ variables.php
@@ -0,0 +1,14 @@
+<?php
+
+$nombre = 'Andres';
+$edad = 20;
+$casado = true;
+
+var_dump($nombre);
+echo gettype($nombre);
+echo $nombre;
+echo '<br>';
+echo $edadedad;
+echo '<br>';
+echo $casado;
+?>
