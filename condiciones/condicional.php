db3ffb5661353cbdae61d90f9d2c8b00fe18018d
Andres Villacres <andreslkf14@gmail.com>
2023-01-29 21:52:20 -0500

    Condiciones

---
+++ condiciones/condicional.php
@@ -0,0 +1,8 @@
+<?php
+$pais = 'Colombia';
+
+if($pais == 'Mexico' || $pais == 'Espania' || $pais == 'Colombia')
+    echo "aqui se habla espaniol";
+else
+    echo "aqui se habla otro idioma";
+?>
---
+++ condiciones/opTernario.php
@@ -0,0 +1,10 @@
+<?php
+//para que un alumno gane una asignatura debe
+//sacar mas de 3 en la nota definitiva
+//caso contrario pierde la asignatura
+
+$nota = 4.1;
+
+echo( $nota > 4 ? 'ganaste la asignatura' : 'perdiste la asignatura');
+
+?>
